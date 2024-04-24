e504ff12f5e41409e79ac93faee207e5100d20f9
JesusG0nzalez <alexvelazquez1228@gmail.com>
2024-04-23 21:34:23 -0700

    Update responsive design in Project and Card components

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -9,7 +9,7 @@ export default function Project() {
         <>
         <h3 className="title text-3xl font-bold ">Project Expirences</h3>
 
-        <div className=" grid xl:grid-cols-3 lg:grid-cols-2  grid-flow-row mx-auto  mt-20 w-[80%]">
+        <div className=" grid xl:grid-cols-3 lg:grid-cols-2  grid-flow-row mx-auto  mt-20 md:w-[80%]">
             {ProyectsList.map((proyect , index) => (
                 <CardProjects key={index} project={proyect} />
             ))}
--- src/components/Project/card.tsx
+++ src/components/Project/card.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function CardProjects({project}: any) {
     return(
-    <Link className="max-w-sm mx-1 border rounded-t-lg shadow bg-gray-800 border-gray-700 logo_shadow m-2" to={`/Individual_project/${project.id}`}>
+    <Link className="max-w-sm mx-1 border rounded-t-lg shadow bg-gray-800 border-gray-700 logo_shadow m-2 max-lg:mx-auto" to={`/Individual_project/${project.id}`}>
         
         <picture className="rounded-t-lg h-[186px] flex items-center bg-white">
             <img className="w-full h-full object-cover" src={`/Portafolio/${project.icon}`} alt={project.description}/>
@@ -12,9 +12,9 @@ export default function CardProjects({project}: any) {
     <div className="p-5 text-left">
         
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-white capitalize">{project.title}</h5>
-        <div className="flex items-center mt-2.5 mb-5">
+        <div className="flex items-center mt-2.5 mb-5 flex-wrap">
             {project.technologies.map((technology:any, index:any) => (
-                <span key={index} className="bg-white text-black text-xs font-semibold px-2.5 py-0.5 rounded ms-3"> {technology}</span>
+                <span key={index} className="bg-white text-black mt-2 text-xs font-semibold px-2.5 py-0.5 rounded ms-3"> {technology}</span>
             ))}
         </div>
         
